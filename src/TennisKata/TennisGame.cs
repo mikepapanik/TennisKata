18e84dc0e66d3a02d49fc0ebd4d6424bacfa073c
Michail Papanikolas <papanikprofessional@gmail.com>
2026-05-07 13:26:45 +0300

    Add basic scoring for player one

--- src/TennisKata/TennisGame.cs
+++ src/TennisKata/TennisGame.cs
@@ -2,8 +2,30 @@
 
 public class TennisGame
 {
+    private int _playerOneScore;
+
+    public void PlayerOneScores()
+    {
+        _playerOneScore++;
+    }
+
     public string GetScore()
     {
+        if (_playerOneScore == 1)
+        {
+            return "Fifteen-Love";
+        }
+
+        if (_playerOneScore == 2)
+        {
+            return "Thirty-Love";
+        }
+
+        if (_playerOneScore == 3)
+        {
+            return "Forty-Love";
+        }
+
         return "Love-All";
     }
 }
--- tests/TennisKata.Tests/TennisGameTests.cs
+++ tests/TennisKata.Tests/TennisGameTests.cs
@@ -13,4 +13,43 @@ public class TennisGameTests
 
         Assert.Equal("Love-All", score);
     }
+
+    [Fact]
+    public void PlayerOneScoresFirstPoint_ShouldReturnFifteenLove()
+    {
+        var game = new TennisGame();
+
+        game.PlayerOneScores();
+
+        var score = game.GetScore();
+
+        Assert.Equal("Fifteen-Love", score);
+    }
+
+    [Fact]
+    public void PlayerOneScoresSecondPoint_ShouldReturnThirtyLove()
+    {
+        var game = new TennisGame();
+
+        game.PlayerOneScores();
+        game.PlayerOneScores();
+
+        var score = game.GetScore();
+
+        Assert.Equal("Thirty-Love", score);
+    }
+
+    [Fact]
+    public void PlayerOneScoresThirdPoint_ShouldReturnFortyLove()
+    {
+        var game = new TennisGame();
+
+        game.PlayerOneScores();
+        game.PlayerOneScores();
+        game.PlayerOneScores();
+
+        var score = game.GetScore();
+
+        Assert.Equal("Forty-Love", score);
+    }
 }
